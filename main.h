6604e3b87f299c2bf203d0ec4d6610a2a332c70d
Uwem Dickson <dicksonunique@gmail.com>
2022-10-20 01:56:06 -0700

    adding more flags

--- main.h
+++ main.h
@@ -27,6 +27,10 @@ char *convert_base(unsigned long nb, unsigned int base, int upper);
 char *_strdup(char *str);
 char *convert_base_pointer(unsigned long p);
 
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 /**
  * struct flags_printf - struct conversion to function
  * @c: flag string
